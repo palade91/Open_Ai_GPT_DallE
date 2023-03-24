b8660d5970d07ca8a2b675a79f5f0ed8fb234c08
Catalin Palade <palade91@yahoo.com>
2023-03-24 11:31:37 +0000

    Initial Commit

---
+++ GPT/GPTApp.swift
@@ -0,0 +1,17 @@
+//
+//  GPTApp.swift
+//  GPT
+//
+//  Created by Catalin Palade on 24/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct GPTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
